3050e1d5c3430a28db3ae5d83e12325c16bb40c2
Michael McNichol <mcnichol.m@gmail.com>
2018-06-13 21:52:40 -0500

    React homepage with Pivotal UI

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {DefaultButton} from 'pivotal-ui/react/buttons'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <DefaultButton>Welcome to React</DefaultButton>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
