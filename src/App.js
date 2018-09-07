964ff8022a99ac6542ae909ef7ddeb9a2d2750d8
Michael McNichol <mcnichol.m@gmail.com>
2018-09-07 09:32:02 -0500

    Style min max close Code buttons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 
 import * as Code from 'pivotal-ui/css/code';
 import './css/prism.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="terminal">
-          <ul className="terminal-dots"><li/><li/><li/></ul>
+          <ul className="terminal-dots"><li className="terminal-dot-close"/><li className="terminal-dot-min"/><li className="terminal-dot-max"/></ul>
           <pre>
-            <code className="language-java">{`
+            <code className="language-java">
+{`
 public class McNichol {
   public static void main(){
     System.out.println("Well that's a good start...");
   }
 }
-`}</code>
+`}
+            </code>
           </pre>
         </div>
       </div>
