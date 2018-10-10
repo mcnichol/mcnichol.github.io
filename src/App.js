76b9ad8f1629e57ba985777d483090c0e91875fe
Michael McNichol <mcnichol.m@gmail.com>
2018-10-09 21:54:10 -0500

    Route app to component pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import TitleBar from "./components/TitleBar/TitleBar";
 import MainPage from "./components/MainPage/MainPage";
+import Blog from "./components/Blog/Blog";
+import {Route, Switch} from 'react-router-dom'
+import './css/App.css'
+import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
 
 class App extends Component {
 
@@ -8,7 +13,12 @@ class App extends Component {
         return (
             <div className="App">
                 <TitleBar/>
-                <MainPage/>
+                <Switch>
+                    <Route exact path="/" component={MainPage}/>
+                    <Route path="/blog" component={Blog}/>
+                    <Route path="/project" component={Projects}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
             </div>
         );
     }
